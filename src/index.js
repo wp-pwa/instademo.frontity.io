d6e2005537062ffeb5d4c4386898baa95d03501c
David Arenas <darensr@gmail.com>
2018-12-18 09:06:37 +0100

    fix(styles): change the form width

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const App = () => (
         gap="large"
         justify="start"
         direction="column"
-        width="450px"
+        width="460px"
         margin="medium"
       >
         <Form />
