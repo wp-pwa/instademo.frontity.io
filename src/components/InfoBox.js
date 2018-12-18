a0574d09860e1f92c7fd9b7c365e3035eaaf6ec3
David Arenas <darensr@gmail.com>
2018-12-18 11:29:15 +0100

    refactor(components): adapt components to new status system

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -1,87 +1,73 @@
 import React from 'react';
 import { Box, Text, Paragraph, Anchor } from 'grommet';
-import { StatusInfo, StatusGood, StatusWarning } from 'grommet-icons';
+// import { StatusInfo, StatusGood, StatusWarning } from 'grommet-icons';
 import QRCode from 'qrcode.react';
 import styled from 'styled-components';
 
+import Status from './Status';
 import inject from './inject';
 
-const InfoBox = ({ status, message, demoUrl }) => {
-  let StatusIcon = StatusInfo;
-  let color = 'status-unknown';
-  let info = '';
+import { isWordPress, hasPosts, gettingDemo } from '../store/statuses';
 
-  if (status === 'busy') {
-    info = message;
-  } else if (status === 'ok') {
-    StatusIcon = StatusGood;
-    color = 'status-ok';
-    info = 'Demo ready!';
-  } else if (status === 'error') {
-    StatusIcon = StatusWarning;
-    color = 'status-error';
-    info = 'Oops! something went wrong';
-  }
-
-  return (
-    status !== 'idle' && (
-      <Box
-        gap="medium"
-        pad="40px"
-        round="xsmall"
-        elevation="small"
-        background="white"
-        animation={{ type: 'fadeIn', duration: 300 }}
-      >
-        <Box direction="row" gap="small" align="center">
-          <Box>
-            <StatusIcon size="20px" color={color} />
-          </Box>
-          <Text weight={600} size="small" color={color}>
-            {info}
-          </Text>
+const InfoBox = ({ status, statusWP, statusPosts, statusDemo, demoUrl }) =>
+  status !== 'idle' && (
+    <Box
+      gap="large"
+      pad="40px"
+      round="xsmall"
+      elevation="small"
+      background="white"
+      animation={{ type: 'fadeIn', duration: 300 }}
+    >
+      {status !== 'error' ? (
+        <Box align="start" gap="small">
+          <Status name={isWordPress} status={statusWP} />
+          <Status name={hasPosts} status={statusPosts} />
+          <Status name={gettingDemo} status={statusDemo} />
         </Box>
-        {status !== 'error' ? (
+      ) : (
+        <Status name="Oops! Something went wrong" status="error" />
+      )}
+      {status !== 'error' ? (
+        <Opacity value={status === 'ok' ? 1 : 0.2}>
           <Box align="center" gap="medium">
             <Box width="128px" height="128px">
-              <Opacity value={status === 'ok' ? 1 : 0.5}>
-                <QRCode value={demoUrl || 'DEMO'} />
-              </Opacity>
+              <QRCode value={demoUrl || 'DEMO'} />
             </Box>
-            <Text as="label">
+            <Text as="label" textAlign="center">
               Scan QR code to view this demo on your mobile phone and discover
               more features
             </Text>
           </Box>
-        ) : (
-          <Box gap="medium">
-            <Paragraph size="small" margin={{ vertical: '0' }}>
-              We haven't been able to generate your demo, we will get back to
-              you by <strong>email</strong> shortly
-            </Paragraph>
-            <Paragraph size="small" margin={{ vertical: '0' }}>
-              Make sure your WordPress is a <strong>blog</strong> or{' '}
-              <strong>news</strong> site. We only support sites using
-              posts/pages.
-            </Paragraph>
-            <Paragraph size="small" margin={{ vertical: '0' }}>
-              In the meantime, you can visit{' '}
-              <Anchor href="https://blog.frontity.com">
-                https://blog.frontity.com
-              </Anchor>{' '}
-              from a mobile phone to see our PWA theme in action.
-            </Paragraph>
-          </Box>
-        )}
-      </Box>
-    )
+        </Opacity>
+      ) : (
+        <Box gap="medium">
+          <Paragraph size="small" margin={{ vertical: '0' }}>
+            We haven't been able to generate your demo, we will get back to you
+            by <strong>email</strong> shortly
+          </Paragraph>
+          <Paragraph size="small" margin={{ vertical: '0' }}>
+            Make sure your WordPress is a <strong>blog</strong> or{' '}
+            <strong>news</strong> site. We only support sites using posts/pages.
+          </Paragraph>
+          <Paragraph size="small" margin={{ vertical: '0' }}>
+            In the meantime, you can visit{' '}
+            <Anchor href="https://blog.frontity.com">
+              https://blog.frontity.com
+            </Anchor>{' '}
+            from a mobile phone to see our PWA theme in action.
+          </Paragraph>
+        </Box>
+      )}
+    </Box>
   );
-};
 
 export default inject(
   ({ store }) => ({
     status: store.status,
-    message: store.message,
+    statusWP: store.statuses.get(isWordPress),
+    statusPosts: store.statuses.get(hasPosts),
+    statusDemo: store.statuses.get(gettingDemo),
     demoUrl: store.demoUrl,
   }),
   InfoBox,
